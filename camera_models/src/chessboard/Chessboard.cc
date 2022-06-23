df84b5003b22aec4a160f290bd8914d587a42322
jiejie567 <498530205@qq.com>
2022-06-23 16:24:32 +0800

    Chessboard.h

--- camera_models/src/chessboard/Chessboard.cc
+++ camera_models/src/chessboard/Chessboard.cc
@@ -2,7 +2,8 @@
 
 #include <opencv2/calib3d/calib3d.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-
+#include <opencv2/imgproc/types_c.h>
+#include <opencv2/calib3d/calib3d_c.h>
 #include "camodocal/chessboard/ChessboardQuad.h"
 #include "camodocal/chessboard/Spline.h"
 
@@ -17,7 +18,7 @@ Chessboard::Chessboard(cv::Size boardSize, cv::Mat& image)
 {
     if (image.channels() == 1)
     {
-        cv::cvtColor(image, mSketch, CV_GRAY2BGR);
+        cv::cvtColor(image, mSketch, cv::COLOR_GRAY2BGR);
         image.copyTo(mImage);
     }
     else
